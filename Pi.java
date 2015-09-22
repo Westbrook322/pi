9ff6ee9c0b069cc764c92e862d31cbf83c7d1c15
Konstantin Skobeltsyn <konstantin@skobeltsyn.com>
2015-09-22 19:32:36 +0300

    Считает норм

--- Pi.java
+++ Pi.java
@@ -1,5 +1,22 @@
-//KV
+import java.util.Random;
 public class Pi{
-
-  
+  public static void main(String[] args){
+    System.out.println("Pi");  
+    Random randomGenerator = new Random();
+    int numOfPoints = 100000000;
+    int i = 0;
+    int numOfCirclePoints = 0;
+    while (i<numOfPoints) {
+      float x = randomGenerator.nextFloat()*20f-10;
+      float y = randomGenerator.nextFloat()*20f-10;
+      float upBorder   = (float)Math.sqrt(100f-x*x);
+      float downBorder = -(float)Math.sqrt(100f-x*x);
+      if (y<=upBorder && y>=downBorder) numOfCirclePoints +=1;
+//      System.out.println(x+" "+y);
+      i=i+1;
+    }
+    System.out.println(numOfCirclePoints);
+    float pi = (float)(numOfCirclePoints*4.f)/numOfPoints;
+    System.out.println("Pi = "+pi);
+  }  
 }
